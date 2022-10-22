a6e83ab994c00bf8c213034fe657b2899ad5d531
hinamei <hina.gntm@gmail.com>
2022-10-22 16:57:25 +0900

    背景地図、ヘッダーを追加した

---
+++ main.js
@@ -0,0 +1,21 @@
+// ベースマップを作成する
+// ここでは3DのOpenStreetMapを表示する
+var map =  new maplibregl.Map({
+    container: 'map',
+    style: 'https://tile2.openstreetmap.jp/styles/osm-bright/style.json',
+    center: [140.44682363, 36.34173699],
+    zoom: 9,
+    hash: true,
+    pitch: 30,
+    localIdeographFontFamily: false
+})
+
+// UIツール
+// 右下のズームレベルの＋−ボタンを表示する
+map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+// 右上の現在位置の取得ボタンを表示する
+map.addControl(new maplibregl.GeolocateControl({positionOptions: {enableHighAccuracy: true},trackUserLocation: true}), 'top-right');
+// 左下の尺度を表示する
+map.addControl(new maplibregl.ScaleControl() );
+
+// TODO: 画面がロードされたら地図にレイヤを追加する
